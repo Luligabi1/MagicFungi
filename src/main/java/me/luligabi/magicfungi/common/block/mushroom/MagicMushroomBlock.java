ebe118772d600660fd1c920e5aa74da0fe49c5d7
Luligabi1 <luligabi111@gmail.com>
2021-06-24 18:30:53 -0300

    Added Impetus, Clypeus and Utilis mushrooms gen

--- src/main/java/me/luligabi/magicfungi/common/block/mushroom/MagicMushroomBlock.java
+++ src/main/java/me/luligabi/magicfungi/common/block/mushroom/MagicMushroomBlock.java
@@ -3,11 +3,14 @@ package me.luligabi.magicfungi.common.block.mushroom;
 import net.minecraft.block.*;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public abstract class MagicMushroomBlock extends MushroomPlantBlock {
@@ -25,6 +28,19 @@ public abstract class MagicMushroomBlock extends MushroomPlantBlock {
     //TODO: Add custom behavior for growing giant Magic Mushrooms
     @Override
     public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) { }
+
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        BlockPos blockPos = pos.down();
+        BlockState blockState = world.getBlockState(blockPos);
+
+        Block[] bannedBlocks = {Blocks.SAND, Blocks.RED_SAND, Blocks.GRAVEL, Blocks.PACKED_ICE};
+
+        if (blockState.isIn(BlockTags.MUSHROOM_GROW_BLOCK)) {
+            return true;
+        } else {
+            return world.getBaseLightLevel(pos, 0) < 13 && this.canPlantOnTop(blockState, world, blockPos) && !Arrays.asList(bannedBlocks).contains(blockState.getBlock());
+        }
+    }
 
     @Override
     public boolean trySpawningBigMushroom(ServerWorld world, BlockPos pos, BlockState state, Random random) {
--- src/main/java/me/luligabi/magicfungi/common/registry/ConfiguredFeatureRegistry.java
+++ src/main/java/me/luligabi/magicfungi/common/registry/ConfiguredFeatureRegistry.java
@@ -9,30 +9,105 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStep;
-import net.minecraft.world.gen.decorator.ChanceDecoratorConfig;
-import net.minecraft.world.gen.decorator.Decorator;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.ConfiguredFeatures;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.RandomPatchFeatureConfig;
 import net.minecraft.world.gen.placer.SimpleBlockPlacer;
 import net.minecraft.world.gen.stateprovider.SimpleBlockStateProvider;
 
-public class ConfiguredFeatureRegistry { // TODO: Add features for all mushrooms to generate.
+public class ConfiguredFeatureRegistry {
 
+    @SuppressWarnings("deprecation")
     public static void init() {
-        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, vivifica.getValue(), Feature.RANDOM_PATCH.configure(ConfiguredFeatureRegistry.VIVIFICA_MUSHROOM_CONFIG).decorate(Decorator.CHANCE.configure(new ChanceDecoratorConfig(5))).repeat(1));
-        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, vivificaJungle.getValue(), Feature.RANDOM_PATCH.configure(ConfiguredFeatureRegistry.VIVIFICA_MUSHROOM_CONFIG_JUNGLE).decorate(Decorator.CHANCE.configure(new ChanceDecoratorConfig(5))).repeat(3));
+        // Impetus
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, impetus.getValue(), Feature.RANDOM_PATCH.configure(ConfiguredFeatureRegistry.IMPETUS_MUSHROOM_CONFIG).decorate(ConfiguredFeatures.Decorators.SQUARE_HEIGHTMAP_SPREAD_DOUBLE.repeat(1)));
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, impetusSavanna.getValue(), Feature.RANDOM_PATCH.configure(ConfiguredFeatureRegistry.IMPETUS_MUSHROOM_CONFIG_SAVANNA).decorate(ConfiguredFeatures.Decorators.SQUARE_HEIGHTMAP_SPREAD_DOUBLE.repeat(3)));
 
-        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.VEGETAL_DECORATION, vivifica);
+        BiomeModifications.addFeature(BiomeSelectors.categories(OVERWORLD_BIOMES), GenerationStep.Feature.VEGETAL_DECORATION, impetus);
+        BiomeModifications.addFeature(BiomeSelectors.categories(Biome.Category.SAVANNA), GenerationStep.Feature.VEGETAL_DECORATION, impetusSavanna);
+
+        // Clypeus
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, clypeus.getValue(), Feature.RANDOM_PATCH.configure(ConfiguredFeatureRegistry.CLYPEUS_MUSHROOM_CONFIG).decorate(ConfiguredFeatures.Decorators.SQUARE_HEIGHTMAP_SPREAD_DOUBLE.repeat(1)));
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, clypeusIcy.getValue(), Feature.RANDOM_PATCH.configure(ConfiguredFeatureRegistry.CLYPEUS_MUSHROOM_CONFIG_ICY).decorate(ConfiguredFeatures.Decorators.SQUARE_HEIGHTMAP_SPREAD_DOUBLE.repeat(3)));
+
+        BiomeModifications.addFeature(BiomeSelectors.categories(OVERWORLD_BIOMES), GenerationStep.Feature.VEGETAL_DECORATION, clypeus);
+        BiomeModifications.addFeature(BiomeSelectors.categories(Biome.Category.ICY), GenerationStep.Feature.VEGETAL_DECORATION, clypeusIcy);
+
+        // Utilis
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, utilis.getValue(), Feature.RANDOM_PATCH.configure(ConfiguredFeatureRegistry.UTILIS_MUSHROOM_CONFIG).decorate(ConfiguredFeatures.Decorators.SQUARE_HEIGHTMAP_SPREAD_DOUBLE.repeat(1)));
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, utilisExtremeHills.getValue(), Feature.RANDOM_PATCH.configure(ConfiguredFeatureRegistry.UTILIS_MUSHROOM_CONFIG_EXTREME_HILLS).decorate(ConfiguredFeatures.Decorators.SQUARE_HEIGHTMAP_SPREAD_DOUBLE.repeat(3)));
+
+        BiomeModifications.addFeature(BiomeSelectors.categories(OVERWORLD_BIOMES), GenerationStep.Feature.VEGETAL_DECORATION, utilis);
+        BiomeModifications.addFeature(BiomeSelectors.categories(Biome.Category.EXTREME_HILLS, Biome.Category.MUSHROOM), GenerationStep.Feature.VEGETAL_DECORATION, utilisExtremeHills);
+
+        // Vivifica
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, vivifica.getValue(), Feature.RANDOM_PATCH.configure(ConfiguredFeatureRegistry.VIVIFICA_MUSHROOM_CONFIG).decorate(ConfiguredFeatures.Decorators.SQUARE_HEIGHTMAP_SPREAD_DOUBLE.repeat(2)));
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, vivificaJungle.getValue(), Feature.RANDOM_PATCH.configure(ConfiguredFeatureRegistry.VIVIFICA_MUSHROOM_CONFIG_JUNGLE).decorate(ConfiguredFeatures.Decorators.SQUARE_HEIGHTMAP_SPREAD_DOUBLE.repeat(3)));
+
+        BiomeModifications.addFeature(BiomeSelectors.categories(OVERWORLD_BIOMES), GenerationStep.Feature.VEGETAL_DECORATION, vivifica);
         BiomeModifications.addFeature(BiomeSelectors.categories(Biome.Category.JUNGLE), GenerationStep.Feature.VEGETAL_DECORATION, vivificaJungle);
+
+        //TODO: Add morbus generation
     }
 
+    // Impetus Mushroom - Regular
+    public static final RegistryKey<ConfiguredFeature<?, ?>> impetus = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
+            new Identifier(MagicFungi.MOD_ID, "impetus_mushroom"));
+
+    private static final RandomPatchFeatureConfig IMPETUS_MUSHROOM_CONFIG = new RandomPatchFeatureConfig.Builder(new SimpleBlockStateProvider(BlockRegistry.IMPETUS_MUSHROOM_BLOCK.getDefaultState()), SimpleBlockPlacer.INSTANCE).tries(4).build();
+
+    // Impetus Mushroom - Savanna Biomes
+    public static final RegistryKey<ConfiguredFeature<?, ?>> impetusSavanna = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
+            new Identifier(MagicFungi.MOD_ID, "impetus_mushroom_savanna"));
+
+    private static final RandomPatchFeatureConfig IMPETUS_MUSHROOM_CONFIG_SAVANNA = new RandomPatchFeatureConfig.Builder(new SimpleBlockStateProvider(BlockRegistry.IMPETUS_MUSHROOM_BLOCK.getDefaultState()), SimpleBlockPlacer.INSTANCE).tries(24).build();
+
+    // Clypeus Mushroom - Regular
+    public static final RegistryKey<ConfiguredFeature<?, ?>> clypeus = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
+            new Identifier(MagicFungi.MOD_ID, "clypeus_mushroom"));
+
+    private static final RandomPatchFeatureConfig CLYPEUS_MUSHROOM_CONFIG = new RandomPatchFeatureConfig.Builder(new SimpleBlockStateProvider(BlockRegistry.CLYPEUS_MUSHROOM_BLOCK.getDefaultState()), SimpleBlockPlacer.INSTANCE).tries(4).build();
+
+    // Clypeus Mushroom - Savanna Biomes
+    public static final RegistryKey<ConfiguredFeature<?, ?>> clypeusIcy = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
+            new Identifier(MagicFungi.MOD_ID, "clypeus_mushroom_icy"));
+
+    private static final RandomPatchFeatureConfig CLYPEUS_MUSHROOM_CONFIG_ICY = new RandomPatchFeatureConfig.Builder(new SimpleBlockStateProvider(BlockRegistry.CLYPEUS_MUSHROOM_BLOCK.getDefaultState()), SimpleBlockPlacer.INSTANCE).tries(24).build();
+
+    // Utilis Mushroom - Regular
+    public static final RegistryKey<ConfiguredFeature<?, ?>> utilis = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
+            new Identifier(MagicFungi.MOD_ID, "utilis_mushroom"));
+
+    private static final RandomPatchFeatureConfig UTILIS_MUSHROOM_CONFIG = new RandomPatchFeatureConfig.Builder(new SimpleBlockStateProvider(BlockRegistry.UTILIS_MUSHROOM_BLOCK.getDefaultState()), SimpleBlockPlacer.INSTANCE).tries(4).build();
+
+    // Utilis Mushroom - Extreme Hills
+    public static final RegistryKey<ConfiguredFeature<?, ?>> utilisExtremeHills = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
+            new Identifier(MagicFungi.MOD_ID, "utilis_mushroom_extreme_hills"));
+
+    private static final RandomPatchFeatureConfig UTILIS_MUSHROOM_CONFIG_EXTREME_HILLS = new RandomPatchFeatureConfig.Builder(new SimpleBlockStateProvider(BlockRegistry.UTILIS_MUSHROOM_BLOCK.getDefaultState()), SimpleBlockPlacer.INSTANCE).tries(24).build();
+
+
+    // Vivifica Mushroom - Regular
     public static final RegistryKey<ConfiguredFeature<?, ?>> vivifica = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
             new Identifier(MagicFungi.MOD_ID, "vivifica_mushroom"));
 
+    private static final RandomPatchFeatureConfig VIVIFICA_MUSHROOM_CONFIG = new RandomPatchFeatureConfig.Builder(new SimpleBlockStateProvider(BlockRegistry.VIVIFICA_MUSHROOM_BLOCK.getDefaultState()), SimpleBlockPlacer.INSTANCE).tries(4).build();
+
+    // Vivifica Mushroom - Jungle Biome
     public static final RegistryKey<ConfiguredFeature<?, ?>> vivificaJungle = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
             new Identifier(MagicFungi.MOD_ID, "vivifica_mushroom_jungle"));
 
-    private static final RandomPatchFeatureConfig VIVIFICA_MUSHROOM_CONFIG = new RandomPatchFeatureConfig.Builder(new SimpleBlockStateProvider(BlockRegistry.VIVIFICA_MUSHROOM_BLOCK.getDefaultState()), SimpleBlockPlacer.INSTANCE).tries(8).build();
-    private static final RandomPatchFeatureConfig VIVIFICA_MUSHROOM_CONFIG_JUNGLE = new RandomPatchFeatureConfig.Builder(new SimpleBlockStateProvider(BlockRegistry.VIVIFICA_MUSHROOM_BLOCK.getDefaultState()), SimpleBlockPlacer.INSTANCE).tries(32).build();
+    private static final RandomPatchFeatureConfig VIVIFICA_MUSHROOM_CONFIG_JUNGLE = new RandomPatchFeatureConfig.Builder(new SimpleBlockStateProvider(BlockRegistry.VIVIFICA_MUSHROOM_BLOCK.getDefaultState()), SimpleBlockPlacer.INSTANCE).tries(24).build();
+
+    // List of Overworld Biome Categories excluding biomes that would be odd for mushrooms to spawn (such as oceans and deserts)
+    private static final Biome.Category[] OVERWORLD_BIOMES = {Biome.Category.PLAINS,
+            Biome.Category.FOREST,
+            Biome.Category.TAIGA,
+            Biome.Category.SAVANNA,
+            Biome.Category.ICY,
+            Biome.Category.SWAMP,
+            Biome.Category.JUNGLE,
+            Biome.Category.UNDERGROUND,
+            Biome.Category.MUSHROOM};
 }
