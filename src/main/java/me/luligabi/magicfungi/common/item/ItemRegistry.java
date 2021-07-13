fd9f3322b45f27e469d3e71d8e705e752f1d917e
Luligabi1 <luligabi111@gmail.com>
2021-07-13 16:23:09 -0300

    Added Impetus, Clypeus, Utilis, Vivifica and Morbus Essence items.

--- src/main/java/me/luligabi/magicfungi/common/item/ItemRegistry.java
+++ src/main/java/me/luligabi/magicfungi/common/item/ItemRegistry.java
@@ -2,6 +2,7 @@ package me.luligabi.magicfungi.common.item;
 
 import me.luligabi.magicfungi.common.MagicFungi;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
@@ -9,14 +10,35 @@ import net.minecraft.util.registry.Registry;
 public class ItemRegistry {
 
     public static void init() {
+        Registry.register(Registry.ITEM, new Identifier(MagicFungi.MOD_ID, "impetus_essence"), IMPETUS_ESSENCE);
+
+        Registry.register(Registry.ITEM, new Identifier(MagicFungi.MOD_ID, "clypeus_essence"), CLYPEUS_ESSENCE);
+
+        Registry.register(Registry.ITEM, new Identifier(MagicFungi.MOD_ID, "utilis_essence"), UTILIS_ESSENCE);
+
+        Registry.register(Registry.ITEM, new Identifier(MagicFungi.MOD_ID, "vivifica_essence"), VIVIFICA_ESSENCE);
+
+        Registry.register(Registry.ITEM, new Identifier(MagicFungi.MOD_ID, "morbus_essence"), MORBUS_ESSENCE);
+
+
         Registry.register(Registry.ITEM, new Identifier(MagicFungi.MOD_ID, "fungi_fertilizer"), FUNGI_FERTILIZER);
 
         Registry.register(Registry.ITEM, new Identifier(MagicFungi.MOD_ID, "guide_book"), GUIDE_BOOK);
     }
 
+    public static final Item IMPETUS_ESSENCE = new Item(new FabricItemSettings().group(MagicFungi.ITEM_GROUP));
+
+    public static final Item CLYPEUS_ESSENCE = new Item(new FabricItemSettings().group(MagicFungi.ITEM_GROUP));
+
+    public static final Item UTILIS_ESSENCE = new Item(new FabricItemSettings().group(MagicFungi.ITEM_GROUP));
+
+    public static final Item VIVIFICA_ESSENCE = new Item(new FabricItemSettings().group(MagicFungi.ITEM_GROUP));
+
+    public static final Item MORBUS_ESSENCE = new Item(new FabricItemSettings().group(MagicFungi.ITEM_GROUP));
+
+
     public static final FungiFertilizerItem FUNGI_FERTILIZER = new FungiFertilizerItem(new FabricItemSettings().group(MagicFungi.ITEM_GROUP));
 
     public static final GuideBookItem GUIDE_BOOK = new GuideBookItem(new FabricItemSettings().rarity(Rarity.UNCOMMON).group(MagicFungi.ITEM_GROUP));
-
 
 }
