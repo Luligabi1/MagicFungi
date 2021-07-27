9d5201da1851cc3a4d1e5142ea6f6d09c2bde842
Luligabi1 <luligabi111@gmail.com>
2021-07-27 15:26:11 -0300

    Added Glyph textures

--- src/main/java/me/luligabi/magicfungi/common/item/glyph/BaseGlyphItem.java
+++ src/main/java/me/luligabi/magicfungi/common/item/glyph/BaseGlyphItem.java
@@ -21,7 +21,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public abstract class BaseGlyphItem extends Item { //TODO: Add enum to specify action type (block, entity, both, etc).
+public abstract class BaseGlyphItem extends Item {
 
     protected SoundEvent soundEvent;
     protected MushroomType mushroomType;
