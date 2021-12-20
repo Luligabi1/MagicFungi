c05d10c498a63d0dc999cfd51078723ad3d6731d
Luligabi1 <luligabi111@gmail.com>
2021-12-20 10:44:59 -0300

    Added alternative particles for the Cadentis Block (WIP)

--- src/main/java/me/luligabi/magicfungi/common/block/misc/CadentisBlock.java
+++ src/main/java/me/luligabi/magicfungi/common/block/misc/CadentisBlock.java
@@ -1,11 +1,13 @@
 package me.luligabi.magicfungi.common.block.misc;
 
-import me.luligabi.magicfungi.common.misc.ParticleRegistry;
+import me.luligabi.magicfungi.common.util.MushroomType;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
@@ -15,10 +17,13 @@ import java.util.Random;
 
 public class CadentisBlock extends Block {
 
+    public static final EnumProperty<MushroomType> MUSHROOM_TYPE = EnumProperty.of("mushroom_type", MushroomType.class);
+
     private static final VoxelShape BOUNDING_SHAPE = Block.createCuboidShape(6.0D, 6.0D, 6.0D, 10.0D, 10.0D, 10.0D);
 
     public CadentisBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(MUSHROOM_TYPE, MushroomType.UTILIS));
     }
 
     @Override
@@ -27,16 +32,29 @@ public class CadentisBlock extends Block {
     }
 
     @Override
-    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return BOUNDING_SHAPE;
-    }
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) { return BOUNDING_SHAPE; }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) { builder.add(MUSHROOM_TYPE); }
+
+
+    /*@Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (world.isClient) {
+            return ActionResult.success(world.isClient());
+        } else {
+            state.get(MUSHROOM_TYPE)
+            player.incrementStat(Stats.USED.getOrCreateStat(GlyphRegistry.CADENTIS_GLYPH));
+            return ActionResult.CONSUME_PARTIAL;
+        }
+    }*/
 
     @Override
     public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
         double x = (double) pos.getX() + 0.5D;
         double y = (double) pos.getY() + 0.5D;
         double z = (double) pos.getZ() + 0.5D;
-        world.addParticle(ParticleRegistry.UTILIS_FLAME, x, y, z, 0.0D, 0.0D, 0.0D);
-        world.addParticle(ParticleTypes.SMOKE, x, y, z, 0.0D, 0.0D, 0.0D);
+        world.addParticle(MushroomType.getParticleEffect(state.get(MUSHROOM_TYPE)), x, y, z, 0.0D, 0.0D, 0.0D);
+        world.addParticle(ParticleTypes.SMOKE, x, y, z, 0.0D, 0.0D, 0.0D); // TODO: Check if there's an alternative particle to use for white smoke.
     }
 }
