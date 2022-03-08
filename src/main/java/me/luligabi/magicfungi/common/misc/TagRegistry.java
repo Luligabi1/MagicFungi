533a2a5b2ae19dabadcf2618ed6ad25f9513e177
Luligabi1 <luligabi111@gmail.com>
2022-03-08 08:43:55 -0300

    Initial 1.18.2 update, now I wait for the terrablender update :)

--- src/main/java/me/luligabi/magicfungi/common/misc/TagRegistry.java
+++ src/main/java/me/luligabi/magicfungi/common/misc/TagRegistry.java
@@ -1,38 +1,38 @@
 package me.luligabi.magicfungi.common.misc;
 
 import me.luligabi.magicfungi.common.MagicFungi;
-import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
-import net.minecraft.tag.Tag;
+import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class TagRegistry {
 
     public static void init() {
-        MOD_MUSHROOMS = TagFactory.ITEM.create(new Identifier(MagicFungi.MOD_ID, "mod_mushrooms"));
+        MOD_MUSHROOMS = TagKey.of(Registry.ITEM_KEY, new Identifier(MagicFungi.MOD_ID, "mod_mushrooms"));
 
-        MORBUS_GRASS_BLOCK_SPREADABLE = TagFactory.BLOCK.create(new Identifier(MagicFungi.MOD_ID, "morbus_grass_block_spreadable"));
-        MORBUS_DIRT_SPREADABLE = TagFactory.BLOCK.create(new Identifier(MagicFungi.MOD_ID, "morbus_dirt_spreadable"));
+        MORBUS_GRASS_BLOCK_SPREADABLE = TagKey.of(Registry.BLOCK_KEY, new Identifier(MagicFungi.MOD_ID, "morbus_grass_block_spreadable"));
+        MORBUS_DIRT_SPREADABLE = TagKey.of(Registry.BLOCK_KEY, new Identifier(MagicFungi.MOD_ID, "morbus_dirt_spreadable"));
 
-        MORBUS_GRASS_SPREADABLE = TagFactory.BLOCK.create(new Identifier(MagicFungi.MOD_ID, "morbus_grass_spreadable"));
-        MORBUS_FERN_SPREADABLE = TagFactory.BLOCK.create(new Identifier(MagicFungi.MOD_ID, "morbus_fern_spreadable"));
-        MORBUS_TALL_GRASS_SPREADABLE = TagFactory.BLOCK.create(new Identifier(MagicFungi.MOD_ID, "morbus_tall_grass_spreadable"));
-        MORBUS_LARGE_FERN_SPREADABLE = TagFactory.BLOCK.create(new Identifier(MagicFungi.MOD_ID, "morbus_large_fern_spreadable"));
+        MORBUS_GRASS_SPREADABLE = TagKey.of(Registry.BLOCK_KEY, new Identifier(MagicFungi.MOD_ID, "morbus_grass_spreadable"));
+        MORBUS_FERN_SPREADABLE = TagKey.of(Registry.BLOCK_KEY, new Identifier(MagicFungi.MOD_ID, "morbus_fern_spreadable"));
+        MORBUS_TALL_GRASS_SPREADABLE = TagKey.of(Registry.BLOCK_KEY, new Identifier(MagicFungi.MOD_ID, "morbus_tall_grass_spreadable"));
+        MORBUS_LARGE_FERN_SPREADABLE = TagKey.of(Registry.BLOCK_KEY, (new Identifier(MagicFungi.MOD_ID, "morbus_large_fern_spreadable")));
 
-        HOST_BIOME_VEGETATION = TagFactory.BLOCK.create(new Identifier(MagicFungi.MOD_ID, "host_biome_vegetation"));
+        HOST_BIOME_VEGETATION = TagKey.of(Registry.BLOCK_KEY, new Identifier(MagicFungi.MOD_ID, "host_biome_vegetation"));
     }
 
-    public static Tag<Item> MOD_MUSHROOMS;
+    public static TagKey<Item> MOD_MUSHROOMS;
 
-    public static Tag<Block> MORBUS_GRASS_BLOCK_SPREADABLE;
-    public static Tag<Block> MORBUS_DIRT_SPREADABLE;
+    public static TagKey<Block> MORBUS_GRASS_BLOCK_SPREADABLE;
+    public static TagKey<Block> MORBUS_DIRT_SPREADABLE;
 
-    public static Tag<Block> MORBUS_GRASS_SPREADABLE;
-    public static Tag<Block> MORBUS_FERN_SPREADABLE;
-    public static Tag<Block> MORBUS_TALL_GRASS_SPREADABLE;
-    public static Tag<Block> MORBUS_LARGE_FERN_SPREADABLE;
+    public static TagKey<Block> MORBUS_GRASS_SPREADABLE;
+    public static TagKey<Block> MORBUS_FERN_SPREADABLE;
+    public static TagKey<Block> MORBUS_TALL_GRASS_SPREADABLE;
+    public static TagKey<Block> MORBUS_LARGE_FERN_SPREADABLE;
 
-    public static Tag<Block> HOST_BIOME_VEGETATION;
+    public static TagKey<Block> HOST_BIOME_VEGETATION;
 
 }
