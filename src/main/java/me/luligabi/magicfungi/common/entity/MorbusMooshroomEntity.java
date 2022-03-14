af121244926ce3245da48293ed81b418cd17ff99
Luligabi1 <luligabi111@gmail.com>
2022-03-14 18:19:14 -0300

    Finalized Morbus Mooshroom behavior (part of #47)

--- src/main/java/me/luligabi/magicfungi/common/entity/MorbusMooshroomEntity.java
+++ src/main/java/me/luligabi/magicfungi/common/entity/MorbusMooshroomEntity.java
@@ -2,48 +2,257 @@ package me.luligabi.magicfungi.common.entity;
 
 import me.luligabi.magicfungi.common.item.ItemRegistry;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.EntityType;
+import net.minecraft.entity.*;
 import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.mob.*;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.mob.Angerable;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsage;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.TimeHelper;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
+import net.minecraft.world.LocalDifficulty;
+import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
-public class MorbusMooshroomEntity extends PathAwareEntity {
+import java.util.UUID;
 
-    public MorbusMooshroomEntity(EntityType<? extends PathAwareEntity> entityType, World world) {
+public class MorbusMooshroomEntity extends HostileEntity implements Angerable {
+
+    public MorbusMooshroomEntity(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
     }
 
+    private static final TrackedData<Boolean> WARNING;
+    private static final float MAX_RANGE = 6.0F;
+    private int warningSoundCooldown;
+    private static final UniformIntProvider ANGER_TIME_RANGE;
+    private int angerTime;
+    @Nullable
+    private UUID targetUuid;
+
     @Override
     protected void initGoals() {
+        super.initGoals();
         this.goalSelector.add(0, new SwimGoal(this));
-        this.goalSelector.add(1, new EscapeDangerGoal(this, 2.0D));
-        this.goalSelector.add(8, new LookAroundGoal(this));
-        initCustomGoals();
+        this.goalSelector.add(1, new AttackGoal());
+        this.goalSelector.add(1, new MorbusMooshroomEscapeDangerGoal());
+        this.goalSelector.add(5, new WanderAroundGoal(this, 1.0D));
+        this.goalSelector.add(6, new LookAtEntityGoal(this, PlayerEntity.class, MAX_RANGE));
+        this.goalSelector.add(7, new LookAroundGoal(this));
+        this.targetSelector.add(1, new MorbusMooshroomRevengeGoal());
+        this.targetSelector.add(2, new ActiveTargetGoal<>(this, LivingEntity.class, 25, true, false, livingEntity -> !(livingEntity instanceof MorbusMooshroomEntity))); // TODO: Check if entity to-be-attacked isn't a wither (skeleton)
+        this.targetSelector.add(3, new UniversalAngerGoal<>(this, false));
     }
 
-    private void initCustomGoals() { // TODO: Add attack goal
-        //this.goalSelector.add(2, new ZombieAttackGoal(this, 1.0D, false));
-        this.goalSelector.add(7, new WanderAroundFarGoal(this, 1.0D));
-        this.targetSelector.add(1, new ActiveTargetGoal<>(this, PlayerEntity.class, true));
-        this.targetSelector.add(2, new ActiveTargetGoal<>(this, ZombieEntity.class, true));
-        this.targetSelector.add(2, new ActiveTargetGoal<>(this, SkeletonEntity.class, true));
-        this.targetSelector.add(3, new ActiveTargetGoal<>(this, EndermanEntity.class, true));
-        this.targetSelector.add(5, new ActiveTargetGoal<>(this, ZombieVillagerEntity.class, true));
+
+    @Override
+    public void readCustomDataFromNbt(NbtCompound nbt) {
+        super.readCustomDataFromNbt(nbt);
+        this.readAngerFromNbt(this.world, nbt);
     }
 
+    @Override
+    public void writeCustomDataToNbt(NbtCompound nbt) {
+        super.writeCustomDataToNbt(nbt);
+        this.writeAngerToNbt(nbt);
+    }
+
+    @Override
+    public void chooseRandomAngerTime() {
+        this.setAngerTime(ANGER_TIME_RANGE.get(this.random));
+    }
+
+    @Override
+    public void setAngerTime(int ticks) {
+        this.angerTime = ticks;
+    }
+
+    @Override
+    public int getAngerTime() {
+        return this.angerTime;
+    }
+
+    @Override
+    public void setAngryAt(@Nullable UUID uuid) {
+        this.targetUuid = uuid;
+    }
+
+    @Nullable
+    public UUID getAngryAt() {
+        return this.targetUuid;
+    }
+
+    // TODO: Add custom SoundEvents.
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return SoundEvents.ENTITY_COW_AMBIENT;
+    }
+
+    @Override
+    protected SoundEvent getHurtSound(DamageSource source) {
+        return SoundEvents.ENTITY_COW_HURT;
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.ENTITY_COW_DEATH;
+    }
+
+    @Override
+    protected void playStepSound(BlockPos pos, BlockState state) {
+        this.playSound(SoundEvents.ENTITY_COW_STEP, 0.15F, 1.0F);
+    }
+
+    @Override
+    protected float getSoundVolume() {
+        return 0.4F;
+    }
+
+    private void playWarningSound() {
+        if (this.warningSoundCooldown <= 0) {
+            this.playSound(SoundEvents.ENTITY_POLAR_BEAR_WARNING, 1.0F, this.getSoundPitch());
+            this.warningSoundCooldown = 40;
+        }
+
+    }
+
+    @Override
+    protected void initDataTracker() {
+        super.initDataTracker();
+        this.dataTracker.startTracking(WARNING, false);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if (this.warningSoundCooldown > 0) {
+            --this.warningSoundCooldown;
+        }
+
+        if (!this.world.isClient) {
+            this.tickAngerLogic((ServerWorld) this.world, true);
+        }
+
+    }
+
+    @Override
+    public boolean tryAttack(Entity target) {
+        boolean bl = target.damage(DamageSource.mob(this), (float) this.getAttributeValue(EntityAttributes.GENERIC_ATTACK_DAMAGE));
+        if (bl) {
+            this.applyDamageEffects(this, target);
+        }
+
+        return bl;
+    }
+
+
+    public void setWarning(boolean warning) {
+        this.dataTracker.set(WARNING, warning);
+    }
+
+    @Override
+    protected float getBaseMovementSpeedMultiplier() {
+        return 0.98F;
+    }
+
+    @Override
+    public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData, @Nullable NbtCompound entityNbt) {
+        return super.initialize(world, difficulty, spawnReason, entityData, entityNbt);
+    }
+
+    static {
+        WARNING = DataTracker.registerData(MorbusMooshroomEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+        ANGER_TIME_RANGE = TimeHelper.betweenSeconds(20, 39);
+    }
+
+    private class AttackGoal extends MeleeAttackGoal {
+
+        public AttackGoal() {
+            super(MorbusMooshroomEntity.this, 1.25D, true);
+        }
+
+        @Override
+        protected void attack(LivingEntity target, double squaredDistance) {
+            double d = this.getSquaredMaxAttackDistance(target);
+            if (squaredDistance <= d && this.isCooledDown()) {
+                this.resetCooldown();
+                this.mob.tryAttack(target);
+                MorbusMooshroomEntity.this.setWarning(false);
+            } else if (squaredDistance <= d * 2.0D) {
+                if (this.isCooledDown()) {
+                    MorbusMooshroomEntity.this.setWarning(false);
+                    this.resetCooldown();
+                }
+
+                if (this.getCooldown() <= 10) {
+                    MorbusMooshroomEntity.this.setWarning(true);
+                    MorbusMooshroomEntity.this.playWarningSound();
+                }
+            } else {
+                this.resetCooldown();
+                MorbusMooshroomEntity.this.setWarning(false);
+            }
+
+        }
+
+        @Override
+        public void stop() {
+            MorbusMooshroomEntity.this.setWarning(false);
+            super.stop();
+        }
+
+        protected double getSquaredMaxAttackDistance(LivingEntity entity) {
+            return 4.0F + entity.getWidth();
+        }
+    }
+
+    class MorbusMooshroomEscapeDangerGoal extends EscapeDangerGoal {
+
+        public MorbusMooshroomEscapeDangerGoal() {
+            super(MorbusMooshroomEntity.this, 2.0D);
+        }
+
+        @Override
+        public boolean canStart() {
+            return super.canStart() && MorbusMooshroomEntity.this.isOnFire();
+        }
+    }
+
+    class MorbusMooshroomRevengeGoal extends RevengeGoal {
+
+        public MorbusMooshroomRevengeGoal() {
+            super(MorbusMooshroomEntity.this);
+        }
+
+        @Override
+        protected void setMobEntityTarget(MobEntity mob, LivingEntity target) {
+            if (mob instanceof MorbusMooshroomEntity) {
+                super.setMobEntityTarget(mob, target);
+            }
+
+        }
+    }
+
+    @Override
     public ActionResult interactMob(PlayerEntity player, Hand hand) {
         ItemStack itemStack = player.getStackInHand(hand);
-        if (itemStack.isOf(Items.BOWL) && !this.isBaby()) {
+        if (itemStack.isOf(Items.BOWL)) {
             player.playSound(SoundEvents.ENTITY_MOOSHROOM_MILK, 1.0F, 1.0F);
             ItemStack itemStack2 = ItemUsage.exchangeStack(itemStack, player, ItemRegistry.MORBUS_MUSHROOM_STEW.getDefaultStack());
             player.setStackInHand(hand, itemStack2);
@@ -53,22 +262,4 @@ public class MorbusMooshroomEntity extends PathAwareEntity {
         }
     }
 
-    // TODO: Add custom SoundEvents.
-    protected SoundEvent getAmbientSound() {
-        return SoundEvents.ENTITY_COW_AMBIENT;
-    }
-
-    protected SoundEvent getHurtSound(DamageSource source) {
-        return SoundEvents.ENTITY_COW_HURT;
-    }
-
-    protected SoundEvent getDeathSound() {
-        return SoundEvents.ENTITY_COW_DEATH;
-    }
-
-    protected void playStepSound(BlockPos pos, BlockState state) { this.playSound(SoundEvents.ENTITY_COW_STEP, 0.15F, 1.0F); }
-
-    protected float getSoundVolume() {
-        return 0.4F;
-    }
 }
