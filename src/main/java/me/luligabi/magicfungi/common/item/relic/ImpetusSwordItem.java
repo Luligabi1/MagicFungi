1aad86625e645535dd012dee6cb7f15bc70d890e
Luligabi1 <33578169+Luligabi1@users.noreply.github.com>
2022-06-06 10:59:46 -0300

    Finished Impetus Sword's charge attack

--- src/main/java/me/luligabi/magicfungi/common/item/relic/ImpetusSwordItem.java
+++ src/main/java/me/luligabi/magicfungi/common/item/relic/ImpetusSwordItem.java
@@ -1,13 +1,18 @@
 package me.luligabi.magicfungi.common.item.relic;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.SmallFireballEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.world.World;
 
 public class ImpetusSwordItem extends SwordItem implements SpecialChargeRelic {
@@ -18,23 +23,45 @@ public class ImpetusSwordItem extends SwordItem implements SpecialChargeRelic {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        if (world.isClient()) return super.use(world, user, hand);
+        if(world.isClient()) return super.use(world, user, hand);
         ItemStack stack = user.getStackInHand(hand);
 
-        // TODO: Finish charge attack
-        //if (user.isSneaking() && isChargeFull(stack)) {
-            SmallFireballEntity fireball = new SmallFireballEntity(world, user, user.getRotationVector().x, user.getRotationVector().y, user.getRotationVector().z);
-            fireball.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1.5F, 1.0F);
-            world.spawnEntity(fireball);
-            resetCharge(stack);
-        //}
+        if(user.isSneaking() && isChargeFull(stack)) {
+            double x, z;
+            for (int i = 0; i < 360; i += 30) {
+                x = 2 * Math.sin(i);
+                z = 2 * Math.cos(i);
+
+                SmallFireballEntity fireball = new SmallFireballEntity(world, user, x, 0, z) {
+
+                    @Override
+                    protected void onEntityHit(EntityHitResult entityHitResult) {
+                        if (!world.isClient()) {
+                            Entity entity = entityHitResult.getEntity();
+                            if (entity.isFireImmune()) return;
+                            Entity owner = this.getOwner();
+                            int fireTicks = entity.getFireTicks();
+                            entity.setOnFireFor(18);
+                            boolean isFireResistant = entity.damage(DamageSource.fireball(this, owner), 5.0F);
+                            if (!isFireResistant) {
+                                entity.setFireTicks(fireTicks);
+                            } else if (owner instanceof LivingEntity) {
+                                this.applyDamageEffects((LivingEntity) owner, entity);
+                            }
+                        }
+                    }
+                };
+                world.spawnEntity(fireball);
+            }
+        world.playSound(null, user.getBlockPos(), SoundEvents.ENTITY_BLAZE_SHOOT, SoundCategory.PLAYERS, 1F, 1F);
+        resetCharge(stack, user);
+        }
         return TypedActionResult.success(stack);
     }
 
     @Override
     public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         if(!isChargeFull(stack)) increaseCharge(stack);
-
         return super.postHit(stack, target, attacker);
     }
 
