5c59f711a86a9e28f54470a76d9af218aa7ebf97
Luligabi1 <33578169+Luligabi1@users.noreply.github.com>
2022-10-16 21:09:11 -0300

    Small Magical Fungi Armor rendering refactor

--- src/main/java/me/luligabi/magicfungi/client/renderer/item/armor/ArmorMushroomRenderer.java
+++ src/main/java/me/luligabi/magicfungi/client/renderer/item/armor/ArmorMushroomRenderer.java
@@ -13,28 +13,28 @@ import net.minecraft.util.math.Vec3f;
 
 public class ArmorMushroomRenderer  {
 
-    public void renderHelmet(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, LivingEntity playerEntity, int light, BipedEntityModel<LivingEntity> contextModel) {
+    public static void renderHelmet(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, LivingEntity playerEntity, int light, BipedEntityModel<LivingEntity> contextModel) {
         matrixStack.push();
         contextModel.head.rotate(matrixStack);
         matrixStack.translate(0.0D, -0.699999988079071D, -0.20000000298023224D);
         matrixStack.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(-78.0F));
         matrixStack.scale(-1.0F, -1.0F, 1.0F);
         matrixStack.translate(-0.75D, -0.20D, -0.25D);
-        blockRenderManager.renderBlockAsEntity(RED_MUSHROOM_STATE, matrixStack, vertexConsumerProvider, light, LivingEntityRenderer.getOverlay(playerEntity, 0.0F));
+        BLOCK_RENDER_MANAGER.renderBlockAsEntity(RED_MUSHROOM_STATE, matrixStack, vertexConsumerProvider, light, LivingEntityRenderer.getOverlay(playerEntity, 0.0F));
         matrixStack.pop();
     }
 
-    public void renderChestplate(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, LivingEntity playerEntity, int light, BipedEntityModel<LivingEntity> contextModel) {
+    public static void renderChestplate(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, LivingEntity playerEntity, int light, BipedEntityModel<LivingEntity> contextModel) {
         matrixStack.push();
         contextModel.leftArm.rotate(matrixStack);
         matrixStack.translate(0.20000000298023224D, -0.3499999940395355D, 0.5D);
         matrixStack.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(-48.0F));
         matrixStack.scale(-0.5F, -0.5F, 0.5F);
         matrixStack.translate(0.35D, -0.35D, -1.25D);
-        blockRenderManager.renderBlockAsEntity(RED_MUSHROOM_STATE, matrixStack, vertexConsumerProvider, light, LivingEntityRenderer.getOverlay(playerEntity, 0.0F));
+        BLOCK_RENDER_MANAGER.renderBlockAsEntity(RED_MUSHROOM_STATE, matrixStack, vertexConsumerProvider, light, LivingEntityRenderer.getOverlay(playerEntity, 0.0F));
         matrixStack.pop();
     }
 
-    private final BlockRenderManager blockRenderManager = MinecraftClient.getInstance().getBlockRenderManager();
+    private static final BlockRenderManager BLOCK_RENDER_MANAGER = MinecraftClient.getInstance().getBlockRenderManager();
     private static final BlockState RED_MUSHROOM_STATE = Blocks.RED_MUSHROOM.getDefaultState();
 }
--- src/main/java/me/luligabi/magicfungi/mixin/ArmorFeatureRendererMixin.java
+++ src/main/java/me/luligabi/magicfungi/mixin/ArmorFeatureRendererMixin.java
@@ -10,7 +10,6 @@ import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,8 +19,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ArmorFeatureRenderer.class)
 public abstract class ArmorFeatureRendererMixin extends FeatureRenderer<LivingEntity, BipedEntityModel<LivingEntity>> {
 
-    private static final ArmorMushroomRenderer ARMOR_MUSHROOM_RENDERER = new ArmorMushroomRenderer();
-
     private ArmorFeatureRendererMixin(FeatureRendererContext<LivingEntity, BipedEntityModel<LivingEntity>> context) {
         super(context);
     }
@@ -29,12 +26,11 @@ public abstract class ArmorFeatureRendererMixin extends FeatureRenderer<LivingEn
     @Inject(method = "renderArmor", at = @At("HEAD"))
     private void renderArmor(MatrixStack matrices, VertexConsumerProvider vertexConsumers, LivingEntity entity, EquipmentSlot armorSlot, int light, BipedEntityModel<LivingEntity> model, CallbackInfo ci) {
         ItemStack stack = entity.getEquippedStack(armorSlot);
-        Item item = stack.getItem();
-        if (item == ItemRegistry.MAGICAL_FUNGI_HELMET) {
-            ARMOR_MUSHROOM_RENDERER.renderHelmet(matrices, vertexConsumers, entity, light, getContextModel());
+        if (stack.isOf(ItemRegistry.MAGICAL_FUNGI_HELMET)) {
+            ArmorMushroomRenderer.renderHelmet(matrices, vertexConsumers, entity, light, getContextModel());
         }
-        if(item == ItemRegistry.MAGICAL_FUNGI_CHESTPLATE) {
-            ARMOR_MUSHROOM_RENDERER.renderChestplate(matrices, vertexConsumers, entity, light, getContextModel());
+        if(stack.isOf(ItemRegistry.MAGICAL_FUNGI_CHESTPLATE)) {
+            ArmorMushroomRenderer.renderChestplate(matrices, vertexConsumers, entity, light, getContextModel());
         }
     }
 }
